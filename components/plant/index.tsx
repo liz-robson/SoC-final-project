b6337dfff9f12035b9be8474f7fa90882b329901
Liz Robson <lizziearobson@gmail.com>
2023-12-05 13:58:15 +0000

    rendered plant image

--- components/plant/index.tsx
+++ components/plant/index.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import ourPlant from "../../public/assets/ourPlant.png";
+
 export default function Plant() {
-  return <Image src="/../public/assets/ourPlant.png" alt="This is an image of a plant" width={250} height={250}/>;
+  return <Image src={ourPlant} alt="This is an image of a plant" width={250} height={250}/>;
 }
