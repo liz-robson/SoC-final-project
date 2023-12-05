626da26e169209f262c532957792d84cf301b27b
LuisValrod <luisrodriguezvalido@gmail.com>
2023-12-05 12:35:23 +0000

    home img issues

--- components/plant/index.tsx
+++ components/plant/index.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 export default function Plant() {
-  return <image></image>;
+  return <Image src="/../public/assets/ourPlant.png" alt="This is an image of a plant" width={250} height={250}/>;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
-import DelTodo from "../../components/delToDo/index";
+import Plant from "../../components/plant"
+import ListBttn from "../../components/listBttn";
+import Image from "next/image";
+
+const URL =""
 
 export default function Home() {
-  return <DelTodo />;
+  return (
+  <>
+  <Plant />
+  <p>Habit progress</p>
+  <ListBttn />
+
+  </>)
 }
