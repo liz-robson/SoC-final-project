5abc3ebeb9504805e1f6a8c272cd07fb28e8fddf
Ana Raducanu <danuta.anna@yahoo.com>
2023-12-06 15:55:41 +0000

    started the edit and input functionality

--- components/emptyListItem/index.tsx
+++ components/emptyListItem/index.tsx
@@ -1,16 +1,25 @@
 import Image from "next/image";
-import plusIcon from '../../public/icons/empty-list-plus-icon.svg'
-import styles from '../emptyListItem/emptyListItem.module.css'
+import plusIcon from "../../public/icons/empty-list-plus-icon.svg";
+import styles from "../emptyListItem/emptyListItem.module.css";
+import { useState } from "react";
 
-export default function EmptyListItem( {addNewData, taskData} : any) {
+export default function EmptyListItem({ addNewData, taskData }: any) {
+  const [edit, setEdit] = useState(true);
+  const [inputValue, setInputValue] = useState(null);
 
-    return (
-    <div className={styles.emptyListItem}>
-        <Image 
-      src={plusIcon}
-      alt="Plus-icon"
-      height={15}
-      />
-    </div>
-    );
+  function handleEdit() {
+    setEdit(!edit);
   }
+
+  function handleInputValue(e: any) {
+    const { value } = e.target;
+    setInputValue(value);
+  }
+  return edit ? (
+    <div className={styles.emptyListItem} onClick={handleEdit}>
+      <Image src={plusIcon} alt="Plus-icon" height={15} />
+    </div>
+  ) : (
+    <input type="text" onChange={handleInputValue} />
+  );
+}
