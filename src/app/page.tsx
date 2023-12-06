bd7b2b58eed686421b004d184b4f45fb856e1422
Danny Ryan <dannykryan@gmail.com>
2023-12-06 14:25:02 +0000

    added spacing and new containers for header items and plant items

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,21 @@
 import Plant from "../../components/plant"
 import ListBtn from "../../components/listBtn";
 import Link from "next/link";
+import Prompt from "../../components/prompt";
 
 const URL =""
 
 export default function Home() {
   return (
   <>
-  <h1>Habitap</h1>
-  <Plant />
-  <p>Habit progress</p>
+  <div id="header-prompt-container">
+    <h1 id="habitap-header">Habitap</h1>
+    <Prompt />
+  </div>
+  <div id="plant-progress-container">
+    <Plant />
+    <p>Habit progress</p>
+  </div>
   <Link href="/myList"><ListBtn/></Link>
   </>)
 }
