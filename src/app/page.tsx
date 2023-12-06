99088f0844f95b20f1fd1e177a0206e4bcf0eaa5
Ana Raducanu <danuta.anna@yahoo.com>
2023-12-06 12:13:02 +0000

    the plant change based on the score

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,26 @@
-import Plant from "../../components/plant"
+"use client";
+
+import Plant from "../../components/plant";
 import ListBtn from "../../components/listBtn";
 import Link from "next/link";
+import { useState } from "react";
 
-const URL =""
+const URL = "";
 
 export default function Home() {
+  let [score, setScore] = useState(0);
+  const handleClick = () => {
+    setScore(score + 20);
+  };
   return (
-  <>
-  <h1>Habitap</h1>
-  <Plant />
-  <p>Habit progress</p>
-  <Link href="/myList"><ListBtn/></Link>
-  </>)
+    <>
+      <h1 id="habitap-header">Habitap</h1>
+      <Plant score={score} />
+      <p>Habit progress</p>
+      <Link href="/myList">
+        <ListBtn />
+      </Link>
+      <button onClick={handleClick}>Click</button>
+    </>
+  );
 }
