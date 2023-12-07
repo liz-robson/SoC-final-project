e5299c1bde2e195d5164b9b5bcdee19237b57a97
Spencer Ley <spencerley234@gmail.com>
2023-12-07 16:32:03 +0000

    handleClick changes in list item

--- components/listItem/index.tsx
+++ components/listItem/index.tsx
@@ -5,6 +5,7 @@ import styles from '../listItem/listItem.module.css';
 import Image from 'next/image';
 import checkboxTicked from '../../public/icons/checkbox-ticked.svg';
 import checkboxUnticked from '../../public/icons/checkbox-unticked.svg';
+import trashIconRed from '../../public/icons/trash-icon-red.svg'
 
 interface ListItemProps {
   children: ReactNode;
@@ -15,22 +16,36 @@ interface ListItemProps {
 const ListItem: React.FC<ListItemProps> = ({ children, className, todo, armDelete } : any) => {
 
   const [check, setcheck] = useState (todo.completed)
-  function handleClick () {
+  function handleBoxClick () {
   setcheck(!check)
   // todo.completed = !todo.completed
   //  console.log(todo)
-
-    
-
   }
-  return <div className={armDelete ? styles.deleteArmed : styles.todoActive}>
+
+  function handleDeleteClick() {
+    alert(`Deleted ${todo.id}`)
+  }
+
+  return armDelete ? (
+    <div className = {styles.deleteArmed}>
+    {children}
+    <Image 
+      src={trashIconRed}
+      alt={"Armed Delete Button"}
+      height={27} 
+      onClick={handleDeleteClick}/>
+    </div>
+
+  ) : ( 
+  <div className={styles.todoActive}>
     {children}
     <Image 
       src={check ? checkboxTicked : checkboxUnticked}
       alt={check ? "ticked checkbox" : "unticked checkbox"}
       height={27} 
-      onClick={handleClick}/>
-    </div>;
+      onClick={handleBoxClick}/>
+    </div>
+  )
 };
 
 export default ListItem;
