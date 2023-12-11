931508e4a3a1ad619fdc08d385a8e62a5c636064
Liz Robson <lizziearobson@gmail.com>
2023-12-11 14:49:36 +0000

    initial supabase set up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,22 +5,29 @@ import Home from "../../components/homepage/index";
 import MyList from "../../components/myList/index";
 import { useState, useEffect } from "react";
 import MainBtn from "../../components/MainBtn";
-// import { supabase } from "../../lib/initSupabase";
+import { supabase } from "../../lib/initSupabase";
+
+interface Habit {
+  habit_id: string;
+  habit_name: string;
+  
+  completed: boolean;
+}
 
 export default function Parent() {
-  // const [habitTable, setHabitTable] = useState([]);
+  const [habitTable, setHabitTable] = useState([]);
   const [isMyListVisible, setIsMyListVisible] = useState<boolean>(false);
 
-  // const fetchHabits = async () => {
-  //   const { data: habitTable } = await supabase
-  //     .from("habit table")
-  //     .select("*")
-  //     .order("name");
-  //   setHabitTable(habitTable);
-  // };
-  // useEffect(() => {
-  //   fetchHabits();
-  // }, []);
+  const fetchHabits = async () => {
+    const { data: habitTable } = await supabase
+      .from("habit table")
+      .select("*")
+      .order("name");
+    setHabitTable(habitTable);
+  };
+  useEffect(() => {
+    fetchHabits();
+  }, []);
 
   const handleMainBtnClick = () => {
     setIsMyListVisible(!isMyListVisible);
