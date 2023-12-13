fd972a74b383bc59bb038da6d580265e49c8021a
Ana Raducanu <danuta.anna@yahoo.com>
2023-12-13 14:07:55 +0000

    create functionality only one row

--- components/defineRoutine/index.tsx
+++ components/defineRoutine/index.tsx
@@ -1,75 +1,85 @@
-"use client"
+"use client";
 import DelToDoBtn from "../delToDo";
 import MainBtn from "../MainBtn";
 import List from "../routine";
-import {useState} from "react"
-import styles from './page.module.css'
-import { useRouter } from 'next/navigation'
+import { useState } from "react";
+import styles from "./page.module.css";
+import { useRouter } from "next/navigation";
 import supabase from "../../lib/initSupabase";
 
 interface Task {
-  id: number,
-  title: string,
-  completed: boolean
+  id: number;
+  title: string;
+  completed: boolean;
 }
 
-let taskDataOriginal: Task[] = [
-]; 
+let taskDataOriginal: Task[] = [];
 
-export default function RoutineForm({toggleVariable, variable} : any) {
-  let router = useRouter()
+export default function RoutineForm({ toggleVariable, variable }: any) {
+  let router = useRouter();
 
-  const [taskData, setTaskData] = useState<Task[]>(taskDataOriginal)
-  const[toggleData, setToggleData] = useState<any>(true)
+  const [taskData, setTaskData] = useState<Task[]>(taskDataOriginal);
+  const [toggleData, setToggleData] = useState<any>(true);
 
   const addNewData = (todo: Task) => {
-    setTaskData([...taskData, todo])
+    setTaskData([...taskData, todo]);
+  };
+
+  function confirmData() {
+    setToggleData(!toggleData);
+    console.log(taskData);
   }
 
-function confirmData(){
-  setToggleData(!toggleData)
-  console.log(taskData)
-}
+  async function linkToMyList() {
+    const { data, error } = await supabase
+      .from("habit_table")
+      .insert([{ habit_name: taskData[0].title }]);
 
-async function linkToMyList() {
-const { data, error } = await supabase
-  .from('habit_table')
-  .insert([
-    { habit_name: "test"},
-  ])
-  .select()
-  console.log(data)
-  toggleVariable()
-}
+    console.log(data);
+    toggleVariable();
+  }
 
-const deleteData = (id : any) => {
-  const newArray = taskData.filter(task => task.id !== id)
-  setTaskData(newArray)
-}
+  const deleteData = (id: any) => {
+    const newArray = taskData.filter((task) => task.id !== id);
+    setTaskData(newArray);
+  };
 
-  return <>
-    <div >
-      {/* pass down the armDelete to DelToDo */}
-      <div className="pop-up" style={{
-        display: toggleData && "none",
-        position: "absolute",
-        background: "grey",
-        border: "2px solid black",
-        marginLeft: "15%"
-      }}
-        >Are you Sure?
-      <button className={styles.mainBtn} onClick={linkToMyList} >Yes</button>
-      <button className={styles.mainBtn} onClick={confirmData} >No</button>
+  return (
+    <>
+      <div>
+        {/* pass down the armDelete to DelToDo */}
+        <div
+          className="pop-up"
+          style={{
+            display: toggleData && "none",
+            position: "absolute",
+            background: "grey",
+            border: "2px solid black",
+            marginLeft: "15%",
+          }}
+        >
+          Are you Sure?
+          <button className={styles.mainBtn} onClick={linkToMyList}>
+            Yes
+          </button>
+          <button className={styles.mainBtn} onClick={confirmData}>
+            No
+          </button>
+        </div>
+
+        <List
+          taskData={taskData}
+          addNewData={addNewData}
+          deleteData={deleteData}
+        />
       </div>
-
-    <List taskData={taskData} addNewData={addNewData} deleteData={deleteData} />
-  </div>
-  <div className="btn-container"  style={{justifyContent: "center"}}>
-  {/* <MainBtn />
+      <div className="btn-container" style={{ justifyContent: "center" }}>
+        {/* <MainBtn />
   <DelToDoBtn /> */}
-  <button className={styles.mainBtn} onClick={confirmData}>
-      Commit
-    </button>
-  </div>
-</>;
+        <button className={styles.mainBtn} onClick={confirmData}>
+          Commit
+        </button>
+      </div>
+    </>
+  );
 }
