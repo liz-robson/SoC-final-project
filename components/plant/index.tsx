e3d89da9967ce3557d0be12ffd94f22c4f31aff4
Danny Ryan <dannykryan@gmail.com>
2023-12-14 15:59:38 +0000

    bee animations added to Plant

--- components/plant/index.tsx
+++ components/plant/index.tsx
@@ -1,5 +1,8 @@
 import { useState, useEffect } from "react";
 import tallFlower from "../../public/plants/flower-bunch-2.json";
+import threeBeesPlease from "../../public/plants/three-bees-please.json";
+import yourFirstBee from "../../public/plants/your-first-bee.json";
+import twoWholeBees from "../../public/plants/two-whole-bees.json";
 import Lottie from "lottie-react";
 
 interface Plant {
@@ -29,17 +32,26 @@ export default function Plant({ score }: any) {
     // Initial frame values
     let startFrame = tallFlower.ip; //in-point or first frame of animation
     let endFrame = tallFlower.op;   //out-point or last frame of animation
+    let animationData = tallFlower; // Default animation data
+    let loop = false; // Default loop value
 
-    // Determine the frame range based on the score
+    // Determine the frame range and animation data based on the score
     if (score === 0) {
       endFrame /= 9;
     } else if (score == 1) {
       endFrame /= 1.6;
-    } else if (score >= 2) {
-      endFrame /= 1;
+    } else if (score == 2) {
+      animationData = yourFirstBee; // Change animation data for score 2 or more
+      loop = true; // Set loop to true for threeBeesPlease
+    } else if (score == 3) {
+      animationData = twoWholeBees; // Change animation data for score 3 or more
+      loop = true; // Set loop to true for threeBeesPlease
+    } else if (score == 4) {
+      animationData = threeBeesPlease; // Change animation data for score 4 or more
+      loop = true; // Set loop to true for threeBeesPlease
     }
 
-    // Update the state with the new frame range
+    // Update the state with the new frame range, animation data, and loop value
     setDefaultOptions((prevOptions) => ({
       ...prevOptions,
       animationData: {
@@ -47,6 +59,8 @@ export default function Plant({ score }: any) {
         ip: startFrame,
         op: endFrame,
       },
+      animationData, // Set the new animation data
+      loop, // Set the new loop value
     }));
   }, [score]);
 
