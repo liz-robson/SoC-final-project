5b20db8809d3fcb24aade5fab1db08b4ef689854
Danny Ryan <dannykryan@gmail.com>
2023-12-15 14:31:47 +0000

    animation range now works depeneding on score

--- components/plant/index.tsx
+++ components/plant/index.tsx
@@ -18,9 +18,12 @@ interface Plant {
   rendererSettings: {
     preserveAspectRatio: string;
   };
+  inPoint: number;
+  outPoint: number;
 }
 
 export default function Plant({ score }: any) {
+  const [animationKey, setAnimationKey] = useState<number>(0);
   // State to manage animation options
   const [animationOptions, setAnimationOptions] = useState<Plant>({
     speed: 1,
@@ -30,6 +33,8 @@ export default function Plant({ score }: any) {
     rendererSettings: {
       preserveAspectRatio: "xMidYMid slice",
     },
+    inPoint: allFlowers.ip,  // Set the inPoint to the initial in-point
+  outPoint: allFlowers.op, // Set the outPoint to the initial out-point
   });
 
   // Effect to update animation options when the score changes
@@ -42,31 +47,68 @@ export default function Plant({ score }: any) {
 
     // Determine the frame range and animation data based on the score
     if (score === 0) {
-      endFrame /= 9;
+      allFlowers.op = 50;
+      console.log(`score is: ${score}`);
+      console.log(`endFrame is: ${endFrame}`);
     } else if (score == 1) {
-      endFrame /= 1.6;
+      allFlowers.ip = 51;
+      allFlowers.op = 195;
+      console.log(`score is: ${score}`);
+      console.log(`endFrame is: ${endFrame}`);
     } else if (score == 2) {
-      animationData = yourFirstBee; // Change animation data for score 2 or more
-      loop = true; // Set loop to true for threeBeesPlease
+      allFlowers.ip = 196;
+      allFlowers.op = 283;
+      console.log(`score is: ${score}`);
     } else if (score == 3) {
-      animationData = twoWholeBees; // Change animation data for score 3 or more
-      loop = true; // Set loop to true for threeBeesPlease
+      allFlowers.ip = 284;
+      allFlowers.op = 367;
+      console.log(`score is: ${score}`);
     } else if (score == 4) {
-      animationData = threeBeesPlease; // Change animation data for score 4 or more
-      loop = true; // Set loop to true for threeBeesPlease
+      allFlowers.ip = 368;
+      allFlowers.op = 441;
+      console.log(`score is: ${score}`);
+    } else if (score == 5) {
+      allFlowers.ip = 442;
+      allFlowers.op = 496;
+      console.log(`score is: ${score}`);
+    } else if (score == 6) {
+      allFlowers.ip = 497;
+      allFlowers.op = 551;
+      console.log(`score is: ${score}`);
+    } else if (score == 7) {
+      allFlowers.ip = 552;
+      allFlowers.op = 592;
+      console.log(`score is: ${score}`);
+    } else if (score == 8) {
+      animationData = yourFirstBee;
+      loop = true;
+      console.log(`score is: ${score}`);
+    } else if (score == 9) {
+      animationData = twoWholeBees;
+      loop = true;
+      console.log(`score is: ${score}`);
+    } else if (score == 10) {
+      animationData = threeBeesPlease;
+      loop = true;
+      console.log(`score is: ${score}`);
     }
+      
 
     // Update the state with the new frame range, animation data, and loop value
     setAnimationOptions((prevOptions) => ({
       ...prevOptions,
-      animationData, // Set the new animation data
+      animationData,
+      outPoint: endFrame, // Set the new animation data
       loop, // Set the new loop value
     }));
+    // Increment the key to force a re-render and restart the animation
+    setAnimationKey((prevKey) => prevKey + 1);
   }, [score]);
 
   return (
     <>
       <Lottie
+        key={animationKey}
         className={"plant-main"}
         animationData={animationOptions.animationData}
         loop={animationOptions.loop}
